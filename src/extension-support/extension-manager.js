75b721ba22735bd5acc4b13d0331ecfbe98736c0
nmsderp <130254323+nmsderp@users.noreply.github.com>
2023-08-04 20:26:23 -0400

    donald duck 🦆

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -171,7 +171,6 @@ const builtinExtensions = {
     adabrowser: () => require('../extensions/scratch3_adabrowser'),
     vittamicrobit: () => require('../extensions/scratch3_vittamicrobit'),
     adaruntime: () => require('../extensions/scratch3_adaruntime'),
-    posenet2scratch: () => require('../extensions/scratch3_posenet2scratch'),
 
     // snail ide
     snailextras: () => require('../extensions/snail_extras'),
