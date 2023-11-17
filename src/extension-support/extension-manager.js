57839e8404f647d67ffdd3292ae59ec326a39704
nmsderp <130254323+nmsderp@users.noreply.github.com>
2023-11-17 17:34:37 -0500

    tailgaiting momment

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -186,6 +186,7 @@ const builtinExtensions = {
 
     pmCamera: () => require('../extensions/pm_camera'),
     Scratch3WirelessBlocks: () => require('../extensions/scratch3_wireless'),
+    jgTailgating: () => require('../extensions/jg_tailgaiting'),
 
 };
 
